7cbe34b775b2ed1a17fd69a562d70c24c3bcb97d
Finn Lestrange <finn.lestrange@glasgow.ac.uk>
2024-03-22 17:07:05 +0000

    Resolve "Homepage Setup & Navbar"

--- app/views/home.py
+++ app/views/home.py
@@ -1,17 +1,56 @@
 from fastapi import APIRouter
 from fastapi import Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
+
+from pydantic import BaseModel, Field
 
 from app.core.config import templates
 
 router = APIRouter()
 
+
+class userModel(BaseModel):
+    # Sample model to use until we have a database
+    id: int
+    username: str
+    email: str
+    user_type: str
+
+
+users = [
+    userModel(
+        id=1, username="John Doe", email="johndoe@example.com", user_type="staff"
+    ),
+    userModel(
+        id=2, username="Jane Doe", email="janedoe@example.com", user_type="student"
+    ),
+    userModel(
+        id=3, username="John Smith", email="johnsmith@example.com", user_type="staff"
+    ),
+]
+
+
 @router.get("/", response_class=HTMLResponse)
 async def home_view(request: Request):
+    user = None
     return templates.TemplateResponse(
         "pages/index.html",
-        {"request": request},
+        {"request": request, "user": user},
     )
+
+
+# These are testing routes for different page views
+@router.get("/user/{id}", response_class=HTMLResponse)
+async def login(request: Request, id: int):
+    for user in users:
+        if user.id == id:
+            return templates.TemplateResponse(
+                "pages/index.html",
+                {"request": request, "user": user},
+            )
+    else:
+        return RedirectResponse("/", status_code=303)
+
 
 @router.post("/clicked", response_class=HTMLResponse)
 async def clicked_view(request: Request):
--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../app/templates/**/*.{html,js}","../app/src/**/*.{html,js}"],
+  content: [
+    "../app/templates/**/*.{html,js}",
+    "../app/src/**/*.{html,js}",
+    "../app/templates/partials/navigation/*.html",
+  ],
   theme: {
     extend: {},
   },
