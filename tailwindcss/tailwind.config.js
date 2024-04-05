53e6d2704a747e2c7e6a701fbe663b36a19378a0
Finn Lestrange <44287141+71xn@users.noreply.github.com>
2024-04-05 11:45:19 +0100

    updated theme colors to match university branding

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -10,13 +10,49 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ['forest', 'light'], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: 'forest', // name of one of the included themes for dark mode
+    themes: ['forest', 'light',
+      {
+        myPathDark: {
+          "color-scheme": "dark",
+          "primary": "#023660",
+          "primary-content": "#03223b",
+          "secondary": "#068af0",
+          "secondary-content": "#fff",
+          "accent": "#eab218",
+          "neutral": "#2b323b",
+          "neutral-content": "#a0a6b4",
+          "base-100": "#2b323b",
+          "base-200": "#1a1f24",
+          "base-300": "#161a20",
+          "info": "#00aefc",
+          "success": "#00a36c",
+          "warning": "#feb700",
+          "error": "#ff565e",
+        },
+        myPathLight: {
+          "color-scheme": "light",
+          "primary": "#04365e",
+          "primary-content": "#074e88",
+          "secondary": "#377cfb",
+          "secondary-content": "#fff",
+          "accent": "#f4b913",
+          "accent-content": "#000",
+          "neutral": "#333c4d",
+          "neutral-content": "#f9fafb",
+          "base-100": "oklch(100% 0 0)",
+          "base-content": "#333c4d",
+          "info": "#00aefc",
+          "success": "#00a36c",
+          "warning": "#feb700",
+          "error": "#ff565e",
+        }
+      }], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: 'myPathDark', // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
     prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
     logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ":root", // The element that receives theme color CSS variables
+    themeRoot: "*", // The element that receives theme color CSS variables
   },
 }
